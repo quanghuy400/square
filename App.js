89626ace1be22afe9dbdc2786b815c08f7817396
quanghuy400 <nguyenquang2092005@gmail.com>
2026-01-17 14:18:51 +0700

    Update App.js and add screenshot

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+// Component Square
+const Square = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.square}>
+      <Text style={styles.text}>Hello, world</Text>
     </View>
   );
-}
+};
 
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Square />
+    </View>
+  );
+};
+
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  square: {
+    width: 150,
+    height: 150,
+    backgroundColor: '#f74dc4',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
+
+
+
+
